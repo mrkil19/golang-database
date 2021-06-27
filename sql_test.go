af48bf366f3873dbc85c40cc0d5e8affefcb104a
mutawakkil1911 <mutawakkilalallahrodriguez25@gmail.com>
2021-06-27 14:52:57 +0700

    Belajar golang database Tahap II

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -88,4 +89,140 @@ func TestQueryComplex(t *testing.T) {
 		fmt.Println("Bergabung : ", created_at)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "pichu"
+	password := "252"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx,script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Suskses login", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "pichu"
+	password := "252"
+
+	script := "INSERT INTO user (username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx,script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Berhasil Menambahkan User Baru")
+}
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "alfi.nd@gmail.com"
+	comment := "Semangat abang, keep u spirit"
+
+	script := "INSERT INTO comments (email, comment) VALUES(?, ?)"
+	result, err := db.ExecContext(ctx,script, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Berhasil Menambahkan Komentar dengan ID", insertId)
+}
+
+func TestPrepareStatement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	script := "INSERT INTO comments (email, comment) VALUES(?, ?)"
+	statement, err := db.PrepareContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer statement.Close()
+
+	for i := 0; i < 10; i++ {
+		email := "alfi.nd. " + strconv.Itoa(i)+ "@gmail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+
+		result, err := statement.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Komentar Id", id)
+	}
+}
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	script := "INSERT INTO comments (email, comment) VALUES(?, ?)"
+	for i := 0; i < 10; i++ {
+		email := "alfi.nd. " + strconv.Itoa(i)+ "@gmail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Komentar Id", id)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+
+
 
